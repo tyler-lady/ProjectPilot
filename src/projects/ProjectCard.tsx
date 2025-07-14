92e9cb955bae072407bd3e221ea214e747fa1c4f
tyler-lady <tclady53@gmail.com>
2025-07-14 17:26:58 -0400

    Began implementing ability to edit projects

--- src/projects/ProjectCard.tsx
+++ src/projects/ProjectCard.tsx
@@ -10,6 +10,11 @@ interface ProjectCardProps {
 
 function ProjectCard(props: ProjectCardProps) {
     const { project } = props;
+
+    const handleEditClick = (projectBeingEdited: Project) => {
+        console.log(projectBeingEdited);
+    }
+
     return (
         <div className="card">
             <img src={project.imageUrl} alt={project.name} />
@@ -19,6 +24,10 @@ function ProjectCard(props: ProjectCardProps) {
                 </h5>
                 <p>{formatDescription(project.description)}</p>
                 <p>Budget : {project.budget.toLocaleString()}</p>
+                <button className="bordered" onClick={() => { handleEditClick(project) }}>
+                    <span className="icon-edit"></span>
+                    Edit
+                </button>
             </section>
         </div>
     )
