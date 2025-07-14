f41b3c66fb946c246114c2fee96b420d6dc82320
tyler-lady <tclady53@gmail.com>
2025-07-14 14:40:44 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ProjectsPage from './projects/ProjectsPage';
+
+function App() {
+  return (
+    <div className="container">
+      <ProjectsPage />
+    </div>
+  );
+}
+
+export default App
